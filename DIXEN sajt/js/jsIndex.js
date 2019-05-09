e2076527e80eaef8f21d0ac4b8c119e39a68c7a2
Bokkacheck <bokis97@live.com>
2019-05-09 16:40:25 +0200

    responsiv-poboljasnje-1

--- DIXEN sajt/js/jsIndex.js
+++ DIXEN sajt/js/jsIndex.js
@@ -1,7 +1,7 @@
-var visinaFooter = parseInt($('footer').height()) + 100;
+var visinaFooter = parseInt($('footer').height()+20);
 var sirinaFuter = parseInt($('footer').width());
-$('footer:nth-of-type(2)').css('clip', 'rect(0px,' + sirinaFuter + 10 + 'px,' + visinaFooter + 'px,' + sirinaFuter / 2 + 'px)');
-$('footer:nth-of-type(1)').css('clip', 'rect(0px,' + sirinaFuter / 2 + 'px,' + visinaFooter + 'px,0px)');
+$('footer:nth-of-type(2)').css('clip', 'rect(0px,' + sirinaFuter+100 + 'px,' + visinaFooter + 'px,' + sirinaFuter / 2 + 'px)');
+$('footer:nth-of-type(1)').css('clip', 'rect(0px,' + sirinaFuter / 2 + 'px,' + visinaFooter+ 10 + 'px,0px)');
 
 $('nav:nth-of-type(1)').removeClass('d-none');
 var visinaNavigacije = $('nav:nth-of-type(1)').css('height');
@@ -173,9 +173,10 @@ function skrolovanje() {
         labelaSliderAnimacija = false;
     }
     if (proveraVrh(document.querySelector('footer:nth-of-type(1)'))) {
-        $('footer:nth-of-type(1)').animate({ left: '0%', opacity: 1 }, 1500, function () {
+        $('footer:nth-of-type(1)').animate({ left: '0px', opacity: 1 }, 1500, function () {
             $('footer').animate({ opacity: '1' }, { duration: 500, queue: false });
-            $('footer:nth-of-type(1)').css('clip', 'rect(0px,' + sirinaFuter + 'px,' + visinaFooter + 'px,0px)')
+            $('footer:nth-of-type(1)').css('clip', 'rect(0px,' + 2*sirinaFuter + 'px,' + 2*visinaFooter + 'px,0px)');
+            $('footer:nth-of-type(2)').addClass("d-none");
         })
         $('footer:nth-of-type(2)').animate({ left: '0%', opacity: 1 }, 1500);
     }
